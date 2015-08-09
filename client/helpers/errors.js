66d3979411d2e21e28eb08fa9bcf84bed18a4289
Jared Hanstra <jhanstra@gmail.com>
2015-08-08 22:13:29 -0500

    stuff

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -6,6 +6,17 @@ Template.errors.helpers({
 	}
 })
 
+Template.error.rendered = function () {
+	var error = this.data;
+	Meteor.defer(function() {
+		Errors.update(error._id, {$set: {seen: true}});
+	});
+};
+
 throwError = function(message) {
-	Errors.insert({message: message})
+	Errors.insert({message: message, seen: false})
+}
+
+clearErrors = function() {
+	Errors.remove({seen: true});
 }
